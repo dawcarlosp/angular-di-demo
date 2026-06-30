07fe7df9f9b2ed0d5fd0f03fd2f590adb2128ab3
Carlos P. <dawcarlospereira@gmail.com>
2026-06-30 11:55:37 +0200

    El componente a gestiona la suscripcion de forma manual, con fines académicos

--- src/app/components/panel-a/panel-a.ts
+++ src/app/components/panel-a/panel-a.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit} from '@angular/core';
 import { AsyncPipe } from '@angular/common';
 import { ContadorService } from '../../services/contador';
-
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-panel-a',
   standalone: true,
@@ -9,7 +9,18 @@ import { ContadorService } from '../../services/contador';
   templateUrl: './panel-a.html',
   styleUrl: './panel-a.css'
 })
-export class PanelAComponent {
+export class PanelAComponent implements OnInit, OnDestroy{
+    valorContador: number = 0;
+  private sub!: Subscription; 
 
   constructor(public contadorService: ContadorService) {}
+
+  ngOnDestroy(): void {
+     this.sub.unsubscribe();
+  }
+  ngOnInit(): void {
+       this.sub = this.contadorService.contador$.subscribe(valor => {
+      this.valorContador = valor;
+    });
+  }
 }
